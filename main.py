573e541828d21a0c363d84f3d71ef64d5027331e
xiaoni0v0 <nyj@163.com>
2023-05-14 22:52:57 +0800

    copier_server v2.1 maybe completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shutil
 import socket
 import sqlite3
@@ -7,24 +8,20 @@ import threading
 import time
 import tkinter as tk
 from tkinter import ttk
-from typing import Tuple, List, Dict, Callable, Union
+from typing import List, Dict, Callable, Union
+
+from _secret import *
 
 # 常量区
 PORT = 56789
-SECRET_KEY: Tuple[bytes, ...] = (
-    '12300000'.encode(),
-    'super_pw'.encode()
-)
-VERSION: Dict[bytes, Tuple[int, int, int]] = {
-    bytes([0b10000101]): (2, 1, 1)
-}
 SAVE_FILE_PATH = './file/%s'
 DATABASE_PATH = './copier_server_database.db'
+RE_MD5 = re.compile(r'^[0-9A-F]{32}$', re.IGNORECASE)
 
 
 def change_stdout(status: bool):
     """
-    False 为恢复，True 为切换为。。。
+    False 为恢复, True 为切换为。。。
     :param status: 状态
     """
     if status:
@@ -36,16 +33,7 @@ def change_stdout(status: bool):
 
 
 class App:
-    # 单例模式
-    __instance = None
-
-    def __new__(cls):
-        if cls.__instance is None:
-            cls.__instance = super().__new__(cls)
-        return cls.__instance
-
     def __init__(self):
-        self.ts = []
         # 创建套接字
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # 绑定端口
@@ -60,16 +48,15 @@ class App:
         self.root.geometry('1000x500')
         self.root.title('copier_server GUI--by XN')
         #
-        self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(1, weight=1)
-        self.root.rowconfigure(0, weight=1)
         ttk.Sizegrip(self.root).place(relx=1, rely=1, anchor='se')
+        # P
+        self.paned = tk.PanedWindow(self.root, showhandle=True, sashrelief="sunken")
+        self.paned.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
         # -------------------- 左侧信息 --------------------
         # 总框架
-        self.frame1 = tk.Frame(self.root)
-        self.frame1.grid(row=0, column=0, sticky='nesw', padx=10, pady=10)
+        self.frame1 = tk.Frame(self.paned)
         # 上方提示（标签）
-        tk.Label(self.frame1, text='网络连接信息：', font='Consolas').pack(anchor='w')
+        tk.Label(self.frame1, text='网络连接信息:', font='Consolas').pack(anchor='w')
         # 下方关闭服务按钮
         tk.Button(self.frame1, text='关闭服务', font=('Consolas', 20), command=self.close_server).pack(side=tk.BOTTOM)
         # 信息显示框
@@ -80,17 +67,18 @@ class App:
         self.text1 = tk.Text(self.frame11, state=tk.DISABLED, font='Consolas', yscrollcommand=self.scroll1.set)
         self.text1.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
         self.scroll1.config(command=self.text1.yview)
+        # P
+        self.paned.add(self.frame1, minsize=300)
         # -------------------- 右侧交互 --------------------
         # 总框架
-        self.frame2 = tk.Frame(self.root)
-        self.frame2.grid(row=0, column=1, sticky='nesw', padx=10, pady=10)
+        self.frame2 = tk.Frame(self.paned)
         # 上方标签
-        tk.Label(self.frame2, text='交互式管理：', font='Consolas').pack(anchor='w')
+        tk.Label(self.frame2, text='交互式管理:', font='Consolas').pack(anchor='w')
         # 下方输入框
         self.text3 = tk.Entry(self.frame2, font='Consolas')
         self.text3.pack(side=tk.BOTTOM, fill=tk.X)
         self.text3.bind("<Return>", lambda e: self.run_code())
-        tk.Label(self.frame2, text='输入：', font='Consolas').pack(anchor='w', side=tk.BOTTOM)
+        tk.Label(self.frame2, text='输入:', font='Consolas').pack(anchor='w', side=tk.BOTTOM)
         # 信息显示框
         self.frame21 = tk.Frame(self.frame2)
         self.frame21.pack(fill=tk.BOTH, expand=True)
@@ -99,6 +87,8 @@ class App:
         self.text2 = tk.Text(self.frame21, state=tk.DISABLED, font='Consolas', yscrollcommand=self.scroll2.set)
         self.text2.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
         self.scroll2.config(command=self.text2.yview)
+        # P
+        self.paned.add(self.frame2, minsize=300)
 
     def run(self):
         # 先创建一个文件夹用来存文件
@@ -126,8 +116,8 @@ class App:
         except Exception:
             try:
                 exec(code, globals(), globals())
-            except Exception:
-                print('无法解析/运行 "%s"' % code)
+            except Exception as e:
+                print('无法解析/运行 "%s", 原因: %s' % (code, repr(e)))
         else:
             print(repr(res))
         finally:
@@ -151,11 +141,6 @@ class App:
     def close_server(self):
         # 换回来
         change_stdout(False)
-        print('正在关闭线程...')
-        tn = len(self.ts)
-        for ti, t in enumerate(self.ts):
-            print('正在等待 %d/%d 关闭' % (ti + 1, tn))
-            t.join()
         self.server_socket.close()
         print('程序正常退出')
         os._exit(0)
@@ -167,8 +152,7 @@ class App:
             except OSError:
                 continue
             else:
-                self.ts.append(threading.Thread(target=self.treat.main, args=ci))
-                self.ts[-1].start()
+                threading.Thread(target=self.treat.main, args=ci).start()
 
 
 class Treat:
@@ -176,30 +160,40 @@ class Treat:
 
     def __init__(self, app_obj: 'App'):
         self.app_obj = app_obj
-        self.ver_map: Dict[Tuple[int, int, int], Callable[[socket.socket], int]] = {
-            (2, 1, 1): self.version_2_1
+        self.ver_map: Dict[bytes, Callable[[socket.socket], int]] = {
+            bytes([0b10000101]): self.version_2_1
         }
 
     def o(self, formatter: str, *args):
         """
         o: output
         """
-        return self.app_obj.write1('<%s> %s' % (threading.current_thread().name, formatter % args))
+        self.app_obj.write1('%s <%s> %s' % (
+            time.strftime('[%Y-%m-%d %H-%M-%S]', time.localtime()),
+            threading.current_thread().name,
+            formatter % args))
 
     def main(self, cs: socket.socket, ca: Tuple[str, int]):  # cs: client_socket, ca: client_addr
+        # 过滤掉非法访问
+        if ca[0] not in WHITE_LIST_IP:
+            cs.close()
+            self.o('该地址 %s 不在白名单里, 已拒绝连接! \n%s\n', ca[0], '-' * 40)
+            return
         self.o('已连接! %s\n', ca)
         # 接收的消息
         # 1 B -> version
         ver = cs.recv(1)
         self.o('-> version: %s\n', ver)
-        if ver in VERSION:
-            if self.ver_map[VERSION[ver]](cs) == 0:
+        if ver in self.ver_map:
+            if (signal := self.ver_map[ver](cs)) == 0:
                 self.o('已完成通信!\n%s\n', '-' * 40)
+            else:
+                self.o('未完成通信! 原因: %s\n%s\n', signal, '-' * 40)
         else:
-            self.o('版本不支持，已断开连接\n%s\n', '-' * 40)
+            self.o('版本不支持, 已断开连接!\n%s\n', '-' * 40)
         cs.close()
 
-    def version_2_1(self, cs: socket.socket) -> int:
+    def version_2_1(self, cs: socket.socket) -> Union[int, str]:
         try:
             # 1 B -> what
             # 8 B -> sk
@@ -216,7 +210,8 @@ class Treat:
             elif recv['what'] == b'\x01':  # 通信
                 if not sk_correct:
                     cs.close()
-                    return -1
+                    self.o('-> sk: %s (密码不正确)\n', recv['sk'])
+                    return '密码不正确'
                 self.o('-> what: %s (传输)\n', recv['what'])
                 recv['MD5'] = cs.recv(16)
                 recv['name_size'] = cs.recv(2)
@@ -224,23 +219,23 @@ class Treat:
                 # 接收数据的处理
                 md5 = ''.join([self.hash_map[x >> 4 & 0xf] + self.hash_map[x & 0xf] for x in recv['MD5']])
                 self.o('-> MD5: %s\n', md5)
-                if len(md5) != 32:
-                    raise
+                if not RE_MD5.match(md5):
+                    return 'MD5值不正确'
                 name_size = self.bytes_2_int(recv['name_size'])
                 content_size = self.bytes_2_int(recv['content_size'])
                 self.o('-> name_size: %d\n', name_size)
                 self.o('-> content_size: %d\n', content_size)
                 # 接收文件名
                 recv['name'] = cs.recv(name_size)
-                name = recv['name'].decode('UTF-8')
+                name = recv['name'].decode('GBK', errors='ignore')
                 _t = time.time()
                 path = SAVE_FILE_PATH % (time.strftime('[%Y-%m-%d %H-%M-%S] %%s', time.localtime(_t)) % name)
                 self.o('-> name: %s\n', name)
                 self.o('-> file_path (on server): %s\n', path)
-                # 查找数据库，看看是否已经存在
+                # 查找数据库, 看看是否已经存在
                 query = self.app_obj.db.query_one_data(md5)
-                if query is None:  # 还没存，这就存下
-                    self.o('-> 服务器中还没有，这就存下\n')
+                if query is None:  # 还没存, 这就存下
+                    self.o('-> 服务器中还没有, 这就存下\n')
                     cs.send(b'\x01')
                     self.app_obj.db.add_data(md5, _t, path)
                     already_recv = 0
@@ -256,10 +251,7 @@ class Treat:
                     os.rename(path_origin, path)  # 文件重命名
                     self.app_obj.db.set_data(md5, _t, path)  # 修改数据库
         except Exception as e:
-            change_stdout(False)
-            print('通信失败: %s, 已断开连接!\n' % e)
-            change_stdout(True)
-            return -1
+            return repr(e)
         else:
             return 0
 
@@ -274,7 +266,7 @@ class Treat:
 
 class DataBase:
     """
-    每进行一次操作，都要先连接、操作、最后关闭数据库。虽然消耗资源，但是...方便！
+    每进行一次操作, 都要先连接、操作、最后关闭数据库。虽然消耗资源, 但是...方便!
     """
 
     def __init__(self, db_path: str):
@@ -282,8 +274,8 @@ class DataBase:
 
     def create_table(self) -> int:
         """
-        创建表。
-        :return: 0 表示创建成功，-1 表示创建失败
+        创建表
+        :return: 0 表示创建成功, -1 表示创建失败
         """
         # 连接数据库
         con = sqlite3.connect(self.path)
@@ -306,7 +298,7 @@ class DataBase:
     def add_data(self, md5: str, save_time: Union[int, float], file_path: str) -> int:
         """
         添加数据
-        :return: 0 表示插入数据成功，-1 表示插入失败（很有可能是重复主键）
+        :return: 0 表示插入数据成功, -1 表示插入失败（很有可能是重复主键）
         """
         save_time = int(save_time)
         # 连接数据库
@@ -315,7 +307,7 @@ class DataBase:
         sql = '''INSERT INTO copier_server (MD5, save_time, file_path) VALUES (?,?,?);''', (md5, save_time, file_path)
         try:
             cur.execute(*sql)
-            # 千万别忘了提交，不然像我一样排查半天找不到错误原因
+            # 千万别忘了提交, 不然像我一样排查半天找不到错误原因
             con.commit()
             return 0
         except Exception as e:
@@ -328,7 +320,7 @@ class DataBase:
     def delete_data(self, md5: str) -> int:
         """
         删除该项数据
-        :return: 0 表示删除数据成功，-1 表示删除失败（很有可能是数据库内没有该主键）
+        :return: 0 表示删除数据成功, -1 表示删除失败（很有可能是数据库内没有该主键）
         """
         # 连接数据库
         con = sqlite3.connect(self.path)
@@ -336,11 +328,11 @@ class DataBase:
         sql = '''DELETE FROM copier_server WHERE MD5 = ?;''', (md5,)
         try:
             cur.execute(*sql)
-            # 千万别忘了提交，我然像我一样排查半天找不到错误原因
+            # 千万别忘了提交, 我然像我一样排查半天找不到错误原因
             con.commit()
             return 0
         except Exception as e:
-            print('[DataBase] 删除数据失败：', e)
+            print('[DataBase] 删除数据失败:', e)
             return -1
         finally:
             cur.close()
@@ -349,7 +341,7 @@ class DataBase:
     def set_data(self, md5: str, save_time: Union[int, float], file_path: str) -> int:
         """
         修改该项数据
-        :return: 0 表示修改数据成功，-1 表示修改失败（很有可能是数据库内没有该主键）
+        :return: 0 表示修改数据成功, -1 表示修改失败（很有可能是数据库内没有该主键）
         """
         save_time = int(save_time)
         # 连接数据库
@@ -358,11 +350,11 @@ class DataBase:
         sql = '''UPDATE copier_server SET save_time = ?, file_path = ? WHERE MD5 = ?;''', (save_time, file_path, md5)
         try:
             cur.execute(*sql)
-            # 千万别忘了提交，我然像我一样排查半天找不到错误原因
+            # 千万别忘了提交, 我然像我一样排查半天找不到错误原因
             con.commit()
             return 0
         except Exception as e:
-            print('[DataBase] 修改数据失败：', e)
+            print('[DataBase] 修改数据失败:', e)
             return -1
         finally:
             cur.close()
